c17d795670a41c08aed2962aca5f55e09219a9d5
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-26 09:36:34 -0700

    Update PyMLB.py

--- PyMLB.py
+++ PyMLB.py
@@ -1,11 +1,11 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-import math
-while True:
-    try:
-        inp1 = int(input("Enter a number -> "))
-        break
-    except ValueError:
-        print("Invald number")
-x = math.pow(inp1, 3)
-print(int(x))
+import datetime
+x = datetime.datetime.now()
+today = datetime.date.today()
+print(x)
+print(today)
+print(x.strftime("%X"))
+print(x.strftime("%m"))
+print(x.strftime("%d"))
+print(x.strftime("%Y"))
