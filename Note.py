dcce05e51c81e7efc6e77043272622269721bacf
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-26 09:01:30 -0700

    New Things

---
+++ Note.py
@@ -0,0 +1,5 @@
+import math
+x = math.ceil(2.8)
+x = math.floor(7.2)
+x = math.sqrt(4.5)
+x = math.pow(6,5)
--- PyMLB.py
+++ PyMLB.py
@@ -1,5 +1,17 @@
 import math
-x = math.ceil(2.8)
-x = math.floor(7.2)
-x = math.sqrt(4.5)
-x = math.pow(6,5)
+while True:
+    try:
+        inp1 = int(input("Enter a number -> "))
+        break
+    except ValueError:
+        print("Invald number")
+
+while True:
+    try:
+        inp2 = int(input("Enter a number -> "))
+        break
+    except ValueError:
+        print("Invald number")
+bikes = inp1 / inp2
+x = math.ceil(bikes)
+print(f"In this town, for every bike that exists there are {x} people.")
