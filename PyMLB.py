550a04a6ac9d135fad4632de3b9c36e3194c2ef7
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-09-26 09:09:42 -0700

    Update PyMLB.py

--- PyMLB.py
+++ PyMLB.py
@@ -5,13 +5,5 @@ while True:
         break
     except ValueError:
         print("Invald number")
-
-while True:
-    try:
-        inp2 = int(input("Enter a number -> "))
-        break
-    except ValueError:
-        print("Invald number")
-bikes = inp1 / inp2
-x = math.ceil(bikes)
-print(f"In this town, for every bike that exists there are {x} people.")
+x = math.pow(inp1, 3)
+print(int(x))
