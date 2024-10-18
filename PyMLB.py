7b93a4ea8908b6dd7c0c2708d893792cd05d0618
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-10-18 09:03:37 -0700

    Update PyMLB.py

--- PyMLB.py
+++ PyMLB.py
@@ -1,9 +1,15 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-name = "Doutree"
-dogs = 3
+name = "Jayson Doutree"
+proj = "Husky"
 location = "Albuquerque, New Mexico"
+location2 = "Los Ranchos De Albuquerque, New Mexico"
+location3 = "Rio Rancho, New Mexico"
+year = 2013
+rennovationDateMonth = 1
+rennovationDateDay = 13
+rennovationDateYear = 2014
+openingTimeHour = 7
+openingTimeMinutes = 23
 
-print("My name is " + name + " and I have "+ str(dogs) + " dogs and live in "+ location) # + approach
-print(f"My name is {name} and I have {dogs} dogs and live in {location}") # F string approach
-print("My name is", name, "and I have", dogs , "dogs and live in", location) # Comma approach
+print (f"In {year}, {name} announced project {proj} which will Rennovate Existing Animal shelters, one at the Cottonwood mall at {location2}, Another at the Coronado Mall in {location}, and the last one in {location3}. The reopening date for all 3 of the shelters is at {openingTimeHour}:{openingTimeMinutes} AM on {rennovationDateMonth}/{rennovationDateDay}/{rennovationDateYear}!")
