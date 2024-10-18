e065669adb05e1822c5ede90e31534c727bb9f20
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-10-18 09:36:10 -0700

    Update Note.py

--- Note.py
+++ Note.py
@@ -35,3 +35,6 @@ location = "Albuquerque, New Mexico"
 print("My name is " + name + " and I have "+ str(dogs) + " dogs and live in "+ location) # + approach
 print(f"My name is {name} and I have {dogs} dogs and live in {location}") # F string approach
 print("My name is", name, "and I have", dogs , "dogs and live in", location) # Comma approach
+
+# Error handling
+
