e82360e0de68805deff369ff6d09143f9cb2545d
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-10-21 08:06:18 -0700

    error handling

--- PyMLB.py
+++ PyMLB.py
@@ -1,15 +1,12 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
-name = "Jayson Doutree"
-proj = "Husky"
-location = "Albuquerque, New Mexico"
-location2 = "Los Ranchos De Albuquerque, New Mexico"
-location3 = "Rio Rancho, New Mexico"
-year = 2013
-rennovationDateMonth = 1
-rennovationDateDay = 13
-rennovationDateYear = 2014
-openingTimeHour = 7
-openingTimeMinutes = 23
+friend = "Taj"
 
-print (f"In {year}, {name} announced project {proj} which will Rennovate Existing Animal shelters, one at the Cottonwood mall at {location2}, Another at the Coronado Mall in {location}, and the last one in {location3}. The reopening date for all 3 of the shelters is at {openingTimeHour}:{openingTimeMinutes} AM on {rennovationDateMonth}/{rennovationDateDay}/{rennovationDateYear}!")
+try:
+  print(brother)
+except:
+  print("A problem occurred") 
+else:
+  print("The code worked without a problem")
+finally:
+  print("Error handling is complete")
